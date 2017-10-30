10b638c38060394dbc1e6863915602564ccf0b1f
opallios <opallios@gmail.com>
2017-10-31 00:49:18 +0900

    ch-02 샘플 소스

---
+++ ch-02/app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var swig = require('swig');
+
+var index = require('./controllers/index');
+var bands = require('./controllers/band');
+var users = require('./controllers/user');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views/pages'));
+var swig = new swig.Swig();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index.show);
+
+// Band 목록과 생성에 대한 라우트 정의하기
+app.get('/bands', bands.list);
+// id로 Band얻기
+app.get('/band/:id', bands.byId);
+// Band 만들기
+app.post('/bands', bands.create);
+// Band 수정하기
+app.put('/bands/:id', bands.update);
+// id로 삭제하기
+app.delete('/band/:id', bands.delete);
+
+// User 목록과 생성에 대한 라우트 정의하기
+app.get('/users', users.list);
+app.post('/users', users.create);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
